4b3a292707d9acdd5866344b1debfe3d5f14e2d9
wsdjx <3064619939@qq.com>
2026-07-13 21:07:48 +0800

    发送消息的请求类,创建会话的请求类,

---
+++ src/main/java/com/qingledger/dto/request/CreateSessionRequest.java
@@ -0,0 +1,21 @@
+package com.qingledger.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class CreateSessionRequest {
+
+    /** 关联账本 ID */
+    @NotNull(message = "账本ID不能为空")
+    @Positive(message = "账本ID必须大于0")
+    private Long ledgerId;
+
+    /** 人设编码 */
+    @NotBlank(message = "人设类型不能为空")
+    @Size(max = 20, message = "人设类型长度不能超过20")
+    private String personaType;
+}
